87b7add83e673ce3a16ade6aecd2d92214b27201
MHindermann <50413752+MHindermann@users.noreply.github.com>
2020-04-06 13:55:53 +0200

    add Utility

--- app/main.py
+++ app/main.py
@@ -14,21 +14,60 @@ MODULE 4: output result as x
 """
 
 """
-MODULE 1
---------
+MODULE 1:
+
 1. define which files can be used and what format is required; provide template
 2. json, excel should be fine for starters
 """
 
+from os import path
+from openpyxl import load_workbook
 
-def open(file) -> Union[list, str]:
-    pass
+DIR = path.dirname(path.abspath(__file__))
 
+
+class Utility:
+    """ Utility functions """
+
+    @classmethod
+    def load_file(cls, filename) -> Optional[ConceptScheme]:
+        """ Load a file from /input """
+
+        # stop if file does not exist
+        filename = path.join(DIR, "input/" + filename)
+        if path.exists(filename) is False:
+            print(f"ERROR: File {filename} does not exist!")
+            return None
+
+        # choose method depending on file type:
+        if filename.endswith(".xlsx"):
+            workbook = load_workbook(filename)
+            return cls.xlsx2jskos(workbook)
+        elif filename.endswith(".json"):
+            pass
+        else:
+            pass
+
+    @classmethod
+    def xlsx2jskos(cls, workbook) -> ConceptScheme:
+        """ Transform XLSX from template to JSKOS """
+
+        scheme = ConceptScheme()
+        for worksheet in workbook:
+            for row in worksheet.iter_rows(min_row=1, min_col=1, max_col=1, values_only=True):
+                if row is None:
+                    continue
+                else:
+                    concept = Concept(preflabel=LanguageMap({"en": row[0]}))  # TODO: automate language detection
+                    scheme.concepts.add(concept)
+        return scheme
 
 def parse(file):
     """ """
     data = open(file)
 
+
+Utility.load_file("owcm_index.xlsx")
 
 # TODO: create concept scheme from input file
 
