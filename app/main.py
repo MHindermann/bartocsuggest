5b292ef52b5a5d26f573aee33991a5d3da15cb75
MHindermann <50413752+MHindermann@users.noreply.github.com>
2020-04-06 11:22:14 +0200

    add Query

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 """ main.py """
 
 from __future__ import annotations
-from typing import Union, Optional, Set
+from typing import Union, Optional, Set, List
 
+import requests
 
 """ outline of modules
 ----------------------
@@ -11,7 +12,6 @@ MODULE 2: send request to FAST
 MODULE 3: analyse result
 MODULE 4: output result as x
 """
-
 
 """
 MODULE 1
@@ -30,8 +30,58 @@ def parse(file):
     data = open(file)
 
 
+# TODO: create concept scheme from input file
 
-#TODO: create concept scheme from input file
+
+""" BARTOC FAST query module
+
+This module sends queries to BARTOC FAST API and retrieves the results.
+"""
+
+FAST_API = "https://bartoc-fast.ub.unibas.ch/bartocfast/api?"
+
+FAST_SOURCES = ["LuSTRE",
+                "GettyULAN",
+                "GettyAAT",
+                "GettyTGN",
+                "AgroVoc",
+                "Bartoc",
+                "data.ub.uio.no",
+                "Finto",
+                "Legilux",
+                "MIMO",
+                "UNESCO",
+                "OZCAR-Theia",
+                "GACS",
+                "51.15.194.251",
+                "UAAV",
+                "HTW-Chur",
+                "FORTH",
+                "Irstea",
+                "DemoVoc",
+                "ScoLOMFR",
+                "lobid-gnd",
+                "Research-Vocabularies-Australia",
+                "Loterre"]
+
+
+class Query:
+    """ A BARTOC FAST query """
+
+    def __init__(self, searchword: str, maxsearchtime: int = 5, disabled: List[str] = None) -> None:
+        self.searchword = searchword
+        self.maxsearchtime = maxsearchtime
+        if disabled is None:
+            self.disabled = ["Research-Vocabularies-Australia", "Loterre"]
+
+    def get(self) -> requests.models.Response:
+        """ Send query as HTTP request to BARTOC FAST API """
+
+        payload = {"searchword": self.searchword,
+                   "maxsearchtime": self.maxsearchtime,
+                   "disabled": self.disabled}
+
+        return requests.get(url=FAST_API, params=payload)
 
 
 """ JSKOS classes 
